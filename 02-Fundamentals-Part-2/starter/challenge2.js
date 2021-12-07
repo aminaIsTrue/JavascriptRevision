30c85c24713c651eb3037bd216c7fa4d8d70a78d
Amina Ouj <aamina.ouj@gmail.com>
2021-12-07 21:59:35 +0100

    revising Arrays

---
+++ 02-Fundamentals-Part-2/starter/challenge2.js
@@ -0,0 +1,13 @@
+const bills = [125, 555, 44];
+
+function calcTip(bill) {
+  return bill >= 50 && bill <= 300 ? (bill * 15) / 100 : (bill * 20) / 100;
+}
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
+const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
+
+console.log(bills);
+console.log(tips);
+console.log(total);
