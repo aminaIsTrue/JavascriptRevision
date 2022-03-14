043270020ae4d3811f9aefb9820eda6a1c01160e
Amina Ouj <aamina.ouj@gmail.com>
2022-03-14 08:03:02 +0100

    More Object methods revision

---
+++ 02-Fundamentals-Part-2/starter/challenge3.js
@@ -0,0 +1,30 @@
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  CalcBMI: function () {
+    this.BMI = this.mass / (this.height * this.height);
+    return this.BMI;
+  },
+};
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  CalcBMI: function () {
+    this.BMI = this.mass / (this.height * this.height);
+    return this.BMI;
+  },
+};
+// calculate the BMI one time and use the result in multiple situations
+const markBMI = mark.CalcBMI();
+const johnBMI = john.CalcBMI();
+if (mark.BMI > john.BMI) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.BMI}) is heigher than ${john.fullName}'s BMI (${john.BMI}) `
+  );
+} else if (john.BMI > mark.BMI) {
+  console.log(
+    `${john.fullName}'s BMI (${john.BMI}) is heigher than ${mark.fullName}'s BMI (${mark.BMI}) `
+  );
+}
