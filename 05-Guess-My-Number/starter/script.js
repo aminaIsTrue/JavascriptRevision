1428319b581b436c3340332d22bcb3b06c39510e
Amina Ouj <aamina.ouj@gmail.com>
2022-03-24 08:10:48 +0100

    More on Refactoring Code base and implementing the DRY principle!

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -3,45 +3,49 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
-
+let highscore = 0;
+function setTextContent(className, textConent) {
+  document.querySelector(className).textContent = textConent;
+}
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   if (score > 1) {
     //not a guess
     if (!guess) {
-      document.querySelector('.message').textContent = 'Not Valid guess :)';
+      // document.querySelector('.message').textContent = 'Not Valid guess :)';
+      setTextContent('.message', 'Not a Valid guess!');
     }
-    //guess right
+    //exact guess
     else if (guess === secretNumber) {
-      document.querySelector('.message').textContent = 'You guessed right :)';
-      document.querySelector('.number').textContent = secretNumber;
+      setTextContent('.message', 'You guessed right :)');
+      setTextContent('.number', secretNumber);
       document.querySelector('body').style.backgroundColor = '#60b347';
+      if (score > highscore) {
+        highscore = score;
+        setTextContent('.highscore', highscore);
+      }
     }
-    //guess less
-    else if (guess < secretNumber) {
-      document.querySelector('.message').textContent = 'Guess too low! :)';
+    //guess different than the secret number
+    else if (guess !== secretNumber) {
+      setTextContent(
+        '.message',
+        guess > secretNumber ? 'Too heigh..!' : 'Too low..!'
+      );
       score--;
-      document.querySelector('.score').textContent = score;
-    }
-    //guess high
-    else if (guess > secretNumber) {
-      document.querySelector('.message').textContent = 'Guess too hight! :)';
-      score--;
-      document.querySelector('.score').textContent = score;
+      setTextContent('.score', score);
     }
   } else {
-    document.querySelector('.message').textContent =
-      'you exceeded the number of tries!';
-    score--;
-    document.querySelector('.score').textContent = score;
+    setTextContent('.message', 'you exceeded the number of tries!');
+    score = 0;
+    setTextContent('.score', score);
   }
 });
 document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number').textContent = '?';
-  document.querySelector('.guess').value = '';
+  setTextContent('.message', 'Start guessing...');
+  setTextContent('.score', score);
+  setTextContent('.number', '?');
+  setTextContent('.guess', '');
   document.querySelector('body').style.backgroundColor = '#222';
 });
