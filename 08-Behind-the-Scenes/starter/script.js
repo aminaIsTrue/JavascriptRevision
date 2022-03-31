a63a9c3d761b7a5a06b9979517979c77275a4daf
Amina Ouj <aamina.ouj@gmail.com>
2022-03-31 09:44:54 +0200

    Understanding Variables' scope, scope chain and variable lookup

---
+++ 08-Behind-the-Scenes/starter/script.js
@@ -0,0 +1,21 @@
+'use strict';
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    const output = `${firstName} you are ${age}, born in ${birthYear}`;
+    console.log(output);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `Oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+  }
+  printAge();
+  return age;
+}
+const firstName = 'Jonas';
+calcAge(1991);
