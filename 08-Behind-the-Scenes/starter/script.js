652b20f12f21711dbea5fe49083537a9ab25dd1b
Amina Ouj <aamina.ouj@gmail.com>
2022-04-02 08:14:04 +0200

    understanding the this keyword in differentcode scopes: global scope, refular function scope,arrow function scope

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -63,16 +63,58 @@
 // and undefined is a falsy expression !undefined = undefined
 // thats why the delete function will be executed
 // SO BEST PRACTICE IS DECLARE FIRST THEN USE!
-if (!numProducts) deleteShoppingCard();
-var numProducts = 10;
-function deleteShoppingCard() {
-  console.log('all products have been deleted!');
-}
-// another DIFFERENCE BETWEEN var let const
+// if (!numProducts) deleteShoppingCard();
+// var numProducts = 10;
+// function deleteShoppingCard() {
+//   console.log('all products have been deleted!');
+// }
+// // another DIFFERENCE BETWEEN var let const
 
-var x = 1;
-let y = 2;
-const z = 3;
-console.log(x === window.x); //  log: true ==> x declared with var is the only variable that creates a prperty in the global window object
-console.log(y === window.y); //log false
-console.log(z === window.z); // log false
+// var x = 1;
+// let y = 2;
+// const z = 3;
+// console.log(x === window.x); //  log: true ==> x declared with var is the only variable that creates a prperty in the global window object
+// console.log(y === window.y); //log false
+// console.log(z === window.z); // log false
+
+// this in the gglobal scope = the window object
+// console.log(this);
+
+// this inside aregular function (function without an owner)=
+// undefined (in strict mode) ||  Golobal Object (in sloppy mode)
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAge(1986);
+
+// this inside arrow function = global object
+// because arrow functions does not get their own this keyword
+// Arrow fuctions use the Lexical this keyword which means that it uses the this keyword of its parent function (its parent scope)
+// const calcAgeArr = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+
+// calcAgeArr(1986);
+
+// this keyword is the Amina Object: the parent Object
+const Amina = {
+  year: 1986,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+Amina.calcAge();
+// if we create an object that borrows a function from another object
+// then the this keyword will point now to the object that calls it
+const matilda = {
+  year: 2012,
+};
+matilda.calcAge = Amina.calcAge;
+matilda.calcAge();
+
+// based on the previous explanation,this keyword is undefined
+const f = Amina.calcAge;
+f();
